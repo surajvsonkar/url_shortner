dbae16a4d816c3c604e227643d1eeabf9b1b30d7
Suraj  Sonkar <surajvirendrasonkar@gmail.com>
2025-06-09 20:29:55 +0530

    creating the proper file structure

---
+++ backend/src/dao/shortUrl.js
@@ -0,0 +1,16 @@
+import urlSchema from "../models/shortUrl.model.js"
+
+export const saveShortUrl = async(shortUrl, longUrl, userId)=> {
+    const newUrl = new urlSchema({
+        full_url: longUrl,
+        short_url: shortUrl
+    })
+    if(userId){
+        newUrl._userId = userId
+    }
+    newUrl.save()
+}
+
+export const getShortUrl = async (shortUrl)=> {
+    return await urlSchema.findOne({short_url: shortUrl})
+}
